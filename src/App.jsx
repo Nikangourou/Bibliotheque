57291c73a6e042e2f59bd853a357b6e16e786973
Remi Venant <remi.venant@univ-lemans.fr>
2022-01-02 00:48:17 +0100

    remove demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,19 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
+// import PropTypes from 'prop-types';
 // import classNames from 'classnames';
-import Container from 'react-bootstrap/Container';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import AppNavbar from './components/AppNavbar';
-import Welcome from './components/Welcome';
-import HousesView from './components/HousesView';
-import InterrogatorView from './components/InterrogatorView';
 import RootStore from './RootStore';
 import STORE from './store';
 
 import './App.scss';
 
-const HousesResume = lazy(() => import(/* webpackChunkName: "bundler-housesResume" */ './components/HousesResume'));
-
 function App() {
   return (
-    <BrowserRouter basename={APP_ENV.APP_PUBLIC_PATH}>
-      <RootStore.Provider value={STORE}>
-        <AppNavbar />
-        <main>
-          <Container fluid>
-            <Routes>
-              <Route path="/" element={<Welcome />} />
-              <Route path="/houses/*" element={<HousesView />} />
-              <Route
-                path="/houses-resume"
-                element={(
-                  <Suspense fallback={<h4>Chargement du composant...</h4>}>
-                    <HousesResume />
-                  </Suspense>
-                )}
-              />
-              <Route path="/interrogator" element={<InterrogatorView />} />
-            </Routes>
-          </Container>
-        </main>
-      </RootStore.Provider>
-    </BrowserRouter>
+    <RootStore.Provider value={STORE}>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </RootStore.Provider>
+
   );
 }
 
--- src/store.js
+++ src/store.js
@@ -1,13 +1,3 @@
-import HouseManager from './model/HouseManager';
-
-const STORE = {
-  houseManager: new HouseManager(),
-  interrogator: null, // sera créé à la demande
-};
-
-// Quelques exemples de maisons
-STORE.houseManager.addHouse('pm', 'Petite maison', [76]);
-STORE.houseManager.addHouse('mm', 'Moyenne maison', [66, 44.5]);
-STORE.houseManager.addHouse('gm', 'Grande maison', [80, 60, 60]);
+const STORE = {};
 
 export default STORE;
